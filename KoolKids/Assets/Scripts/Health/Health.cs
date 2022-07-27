6632afa87180ebc715d5815c94e4f0fd8a7c2110
iRessu <nicklas.haggstrom@hotmail.com>
2022-07-27 11:24:58 +0200

    iFrames

--- KoolKids/Assets/Scripts/Health/Health.cs
+++ KoolKids/Assets/Scripts/Health/Health.cs
@@ -1,24 +1,32 @@
-
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")] 
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
+    private Animator anim;
     private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
-    private void TakeDamage(float _damage)
+    public void TakeDamage(float _damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
         if (currentHealth > 0)
         {
-            //takedamage
+            StartCoroutine(Invunerability());
         }
 
         else
@@ -31,11 +39,25 @@ public class Health : MonoBehaviour
         }
     }
 
-    private void Update()
+    public void AddHealth(float _value)
     {
-        if (Input.GetKeyDown(KeyCode.F))
-            TakeDamage(1);
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
+
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1,0,0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+    }
+
+   
 
 
 
