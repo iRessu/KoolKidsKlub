fe9abb21db60dd3329089ac781333a7efca752e1
iRessu <109947890+iRessu@users.noreply.github.com>
2022-07-28 20:17:03 +0200

    Enemy/Playeranimations

---
+++ KoolKids/Assets/Scripts/Player/PlayerCombat.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    private PlayerMovement playerMovement;
+    const string PLAYER_ATTACK = "Player_Atk";
+    
+
+    void Start()
+    {
+        GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        GetComponent<PlayerMovement>().ChangeAnimationState(PLAYER_ATTACK); 
+    }
+
+
+}
--- KoolKids/Assets/Scripts/Player/PlayerMovement.cs
+++ KoolKids/Assets/Scripts/Player/PlayerMovement.cs
@@ -13,13 +13,32 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
-  
+    public Animator animator;
+    public string currentAnimation;
+    public string currentState; 
+
+
+    //Animation states
+    const string PLAYER_IDLE = "Player_Idle";
+    const string PLAYER_RUN = "Player_walk";
+    const string PLAYER_JUMP = "Player_Jump";
+    public const string PLAYER_ATTACK = "PLayer_Atk";
+    
+
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        
-        if(Input.GetButtonDown("Jump") && IsGrounded())
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -28,12 +47,35 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
+
+       
+     
+
+        if (IsGrounded())
+        {
+            if (rb.velocity.x != 0)
+            {
+                ChangeAnimationState(PLAYER_RUN);
+            }
+            else
+            {
+                ChangeAnimationState(PLAYER_IDLE);
+            }
+        }
+
+        if(IsGrounded() == false)
+        {
+            ChangeAnimationState(PLAYER_JUMP);
+        }
+
         Flip();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+      
+
     }
 
     private bool IsGrounded()
@@ -50,5 +92,14 @@ public class PlayerMovement : MonoBehaviour
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+
+   
+   
+   public void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+
+        animator.Play(newState);
     }
 }
